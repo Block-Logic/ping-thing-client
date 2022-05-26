f1b243dfdf82b27c4058ef30ad9d1f3c27361241
Brian Long <brian.long@fmadata.com>
2022-05-26 06:12:19 -0600

    Bug fixes & comments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ping-thing-client
-Node JS Client to Contribute Ping Times to the Validators.app Ping Thing
+Node JS Client to contribute ping times to the Validators.app Ping Thing.
 
 ## Install notes
 `git clone https://github.com/Block-Logic/ping-thing-client.git`
@@ -10,15 +10,15 @@ Try `yarn install`. If that doesn't work, use:
 `yarn add dotenv`
 `yarn add xhr2`
 
-I use .env to hold sensitive data that I don't want to appear in the Git repo. Copy .env.sample to .env and replace the values inside the file with your data. The .env file needs your private wallet in base58 format. There is a simple Ruby script that will convert a keypair.json file into base58. See keypair_to_base58.rb
+I use .env to hold sensitive data that I don't want to appear in the GitHub repo. Copy .env.sample to .env and replace the values inside the file with your data. The .env file needs your private wallet keypair in base58 format. There is a simple Ruby script that will convert a keypair.json file into base58. See keypair_to_base58.rb
 
 Before you can post pings to validators.app, you will need an API key. You can sign up at https://www.validators.app/users/sign_up and grab a free API key from your dashboard.
 
 After retrieving your API key, copy & paste it into the VA_API_KEY attribute of your .env file.
 
-In the .env file, try `VERBOSE_LOG=true` to see log output the first time you run the script. After saving your .env file, try running the script with `node ping-thing-client.mjs` and watch the output if you set verbose mode = true. I `VERBOSE_LOG=false` in production to minimize log noise.
+In the .env file, try `VERBOSE_LOG=true` to see log output the first time you run the script. After saving your .env file, try running the script with `node ping-thing-client.mjs` and watch the output if you set verbose mode = true. I use `VERBOSE_LOG=false` in production to minimize log noise.
 
 ## Running the Ping Thing Script
 You can start the script & push it to the background with `node ping-thing-client.mjs >> ping-thing.log &`.
 
-Look for a service file in this repo soon.
+Look for an example service file in this repo soon.
--- ping-thing-client.mjs
+++ ping-thing-client.mjs
@@ -1,23 +1,37 @@
 // Sample use:
-// node ping-thing-client.mjs >> ping-thing.log  2>&1 &
+// node ping-thing-client.mjs >> ping-thing.log 2>&1 &
 
 import dotenv from "dotenv";
 import web3 from '@solana/web3.js';
 import bs58 from 'bs58';
 import XMLHttpRequest from 'xhr2';
 
+// Catch interrupts & exit
+process.on(
+  'SIGINT',
+  function() {
+    console.log(`Caught interrupt signal at ${new Date()}`, '\n');
+    process.exit();
+  }
+);
+
 // Read constants from .env
 dotenv.config();
 const RPC_ENDPOINT = process.env.RPC_ENDPOINT;
-const USER_PRIVATE_KEY = bs58.decode(process.env.WALLET_PRIVATE_KEYPAIR);
-const USER_KEYPAIR = web3.Keypair.fromSecretKey(USER_PRIVATE_KEY);
+const USER_KEYPAIR = web3.Keypair.fromSecretKey(
+                       bs58.decode(process.env.WALLET_PRIVATE_KEYPAIR)
+                     );
 const SLEEP_MS = process.env.SLEEP_MS;
 const VA_API_KEY = process.env.VA_API_KEY;
 const VERBOSE_LOG = process.env.VERBOSE_LOG;
 
+// Set up web3 client
 const walletAccount = new web3.PublicKey(USER_KEYPAIR.publicKey);
 const connection = new web3.Connection(RPC_ENDPOINT, 'confirmed');
 const commitmentLevel = 'confirmed';
+
+// Set up our REST client
+const restClient = new XMLHttpRequest();
 
 // Setup our transaction
 var tx = new web3.Transaction();
@@ -36,56 +50,57 @@ var tryCount = 0;
 var maxTries = 3;
 while(true) {
   try {
+    // Send the TX to the cluster
     var txStart = new Date();
-    var signature = await web3.sendAndConfirmTransaction(
-      connection,
-      tx,
-      [USER_KEYPAIR],
-      { commitment: commitmentLevel }
-    );
-    var txEnd = new Date();
+    try {
+      var signature = await web3.sendAndConfirmTransaction(
+        connection,
+        tx,
+        [USER_KEYPAIR],
+        { commitment: commitmentLevel }
+      );
+      var txSuccess = true;
+    } catch (e) {
+      // TODO -- Sample Errors to catch:
+      // TransactionExpiredBlockheightExceededError: Signature vxvewmXNthUacKXHaAu9XyduSgJ5DiVFS2UqzM1Q8z5D1cUZAKvXGZDhzDW9kn2mqADjfwy4iBExE1Je1im72AA has expired: block height exceeded.
+      // SendTransactionError: failed to send transaction: Transaction simulation failed: This transaction has already been processed
+      console.log('\n', e, '\n');
+      var txSuccess = false;
+    } finally {
+      var txEnd = new Date();
+    }
     var txElapsedMs = txEnd - txStart;
 
-    if (VERBOSE_LOG === true) console.log(`tx: ${JSON.stringify(signature)}`);
-
-    const response = new XMLHttpRequest();
+    // prepare the payload to send to validators.app
     const payload = JSON.stringify({
       time: txElapsedMs,
       signature: signature,
       transaction_type: 'transfer',
-      success: true,
+      success: txSuccess,
       application: 'web3',
       commitment_level: commitmentLevel
     });
 
-    if (VERBOSE_LOG === true) console.log(payload, '\n');
+    if (VERBOSE_LOG === true) {
+      console.log(`${new Date().toISOString()} => ${payload}`);
+    }
 
     // Send the ping data to validators.app
-    response.open(
+    restClient.open(
       "POST",
       'https://www.validators.app/api/v1/ping-thing/mainnet'
     );
-    response.setRequestHeader('Content-Type', 'application/json');
-    response.setRequestHeader('Token', VA_API_KEY);
-    response.send(payload);
-
-    // Catch interrupts & exit
-    process.on('SIGINT', function() {
-      console.log(`Caught interrupt signal at ${new Date()}`, '\n');
-      process.exit();
-    });
+    restClient.setRequestHeader('Content-Type', 'application/json');
+    restClient.setRequestHeader('Token', VA_API_KEY);
+    restClient.send(payload);
 
     // Reset the try counter and sleep
     tryCount = 0;
     await new Promise(r => setTimeout(r, SLEEP_MS));
-
-    // TODO -- Sample Errors to catch:
-    // TransactionExpiredBlockheightExceededError: Signature vxvewmXNthUacKXHaAu9XyduSgJ5DiVFS2UqzM1Q8z5D1cUZAKvXGZDhzDW9kn2mqADjfwy4iBExE1Je1im72AA has expired: block height exceeded.
-    // SendTransactionError: failed to send transaction: Transaction simulation failed: This transaction has already been processed
-    // (node:9231) MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 SIGINT listeners added to [process]. Use emitter.setMaxListeners() to increase limit
   } catch (e) {
     console.log('\n', e, '\n');
     if (++tryCount == maxTries) throw e;
   }
 }
+
 if (VERBOSE_LOG === true) console.log(`Ending script at ${new Date()}`, '\n');
