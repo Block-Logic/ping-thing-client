f056b8b963e923f7402790d9c78b52459b100aee
Wilfred Almeida <almeidawilfred642@gmail.com>
2024-03-29 09:57:07 +0530

    refactor: websocket connection, max attempts
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- utils/blockhash.mjs
+++ utils/blockhash.mjs
@@ -1,12 +1,23 @@
 import { sleep } from "./misc.mjs";
 
+const MAX_BLOCKHASH_FETCH_ATTEMPTS = 100;
+let attempts = 0;
+
 export const watchBlockhash = async (gBlockhash, connection) => {
   // const gBlockhash = { value: null, updated_at: 0 };
   while (true) {
     try {
       // Use a 5 second timeout to avoid hanging the script
       const timeoutPromise = new Promise((_, reject) =>
-        setTimeout(() => reject(new Error('Operation timed out')), 5000)
+        setTimeout(
+          () =>
+            reject(
+              new Error(
+                `${new Date().toISOString()} ERROR: Blockhash fetch operation timed out`
+              )
+            ),
+          5000
+        )
       );
       // Get the latest blockhash from the RPC node and update the global
       // blockhash object with the new value and timestamp. If the RPC node
@@ -14,23 +25,33 @@ export const watchBlockhash = async (gBlockhash, connection) => {
       // script will log an error.
       gBlockhash.value = await Promise.race([
         connection.getLatestBlockhash("finalized"),
-        timeoutPromise
+        timeoutPromise,
       ]);
 
       gBlockhash.updated_at = Date.now();
+      attempts = 0;
     } catch (error) {
       gBlockhash.value = null;
       gBlockhash.updated_at = 0;
 
+      ++attempts;
+
       if (error.message.includes("new blockhash")) {
         console.log(
-          `${new Date().toISOString()} ERROR: Unable to obtain a new blockhash`,
+          `${new Date().toISOString()} ERROR: Unable to obtain a new blockhash`
         );
       } else {
         console.log(`${new Date().toISOString()} ERROR: ${error.name}`);
         console.log(error.message);
         console.log(error);
         console.log(JSON.stringify(error));
+      }
+    } finally {
+      if (attempts >= MAX_BLOCKHASH_FETCH_ATTEMPTS) {
+        console.log(
+          `${new Date().toISOString()} ERROR: Max attempts for fetching blockhash reached, exiting`
+        );
+        process.exit(0);
       }
     }
 
--- utils/slot.mjs
+++ utils/slot.mjs
@@ -1,29 +1,47 @@
 import { sleep } from "./misc.mjs";
 
-export const watchSlotSent = async (gSlotSent,connection) => {
+const MAX_SLOT_FETCH_ATTEMPTS = 3;
+let attempts = 0;
+
+export const watchSlotSent = async (gSlotSent, connection) => {
   while (true) {
-    const subscriptionId = connection.onSlotUpdate((value) => {
-      if (value.type === "firstShredReceived") {
-        gSlotSent.value = value.slot;
-        gSlotSent.updated_at = Date.now();
-      }
-    });
+    try {
+      const subscriptionId = connection.onSlotUpdate((value) => {
+        if (value.type === "firstShredReceived") {
+          gSlotSent.value = value.slot;
+          gSlotSent.updated_at = Date.now();
+          attempts = 0;
+        }
+      });
 
-    // do not re-subscribe before first update, max 60s
-    const started_at = Date.now();
-    while (gSlotSent.value === null && Date.now() - started_at < 60000) {
-      await sleep(1);
-    }
-
-    // If update not received in last 3s, re-subscribe
-    if (gSlotSent.value !== null) {
-      while (Date.now() - gSlotSent.updated_at < 3000) {
+      // do not re-subscribe before first update, max 60s
+      const started_at = Date.now();
+      while (gSlotSent.value === null && Date.now() - started_at < 60000) {
         await sleep(1);
       }
-    }
 
-    await connection.removeSlotUpdateListener(subscriptionId);
-    gSlotSent.value = null;
-    gSlotSent.updated_at = 0;
+      // If update not received in last 3s, re-subscribe
+      if (gSlotSent.value !== null) {
+        while (Date.now() - gSlotSent.updated_at < 3000) {
+          await sleep(1);
+        }
+      }
+
+      await connection.removeSlotUpdateListener(subscriptionId);
+      gSlotSent.value = null;
+      gSlotSent.updated_at = 0;
+
+      ++attempts;
+
+      if (attempts >= MAX_SLOT_FETCH_ATTEMPTS) {
+        console.log(
+          `${new Date().toISOString()} ERROR: Max attempts for fetching slot type "firstShredReceived" reached, exiting`
+        );
+        process.exit(0);
+      }
+    } catch (e) {
+      console.log(`${new Date().toISOString()} ERROR: ${e}`);
+      ++attempts;
+    }
   }
-}
+};
